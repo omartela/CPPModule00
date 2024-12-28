370b67a0952394a6a63e4cfb56d5b7897ac92396
omartela <Oskari Martela>
2024-12-28 19:07:13 +0200

    Fixes the ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -20,12 +20,17 @@ int main(int argc, char **argv)
     if (argc < 2)
     {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
     }
-    input = argv[1];
-    for (size_t i = 0; input[i] != '\0'; ++i)
+    for (int i = 1; i < argc; ++i)
     {
-        input[i] = std::toupper(static_cast<unsigned char>(input[i]));
+        input = argv[i];
+        for (size_t j = 0; input[j] != '\0'; ++j)
+        {
+            input[j] = std::toupper(static_cast<unsigned char>(input[j]));
+        }
+        std::cout << input;
     }
-    std::cout << input << std::endl;
+    std::cout << std::endl;
     return (0);
 }
